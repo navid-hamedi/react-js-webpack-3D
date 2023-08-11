21f4e6cc5b6390e476906776a7ddf664b38ab9d4
neanix <nav.hamedi@gmail.com>
2023-08-11 16:58:16 +0200

    three js update

--- src/components/ModelViewer.js
+++ src/components/ModelViewer.js
@@ -2,4 +2,14 @@ import React, { useEffect } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import 'three/examples/jsm/controls/OrbitControls';
-import './ModelViewer.css';
+import '../styles/ModelViewer.scss';
+
+const ModelViewer = () => {
+    useEffect(() => {
+      // Three.js code here
+    }, []);
+  
+    return <div id="model-container"></div>;
+  };
+  
+  export default ModelViewer;
