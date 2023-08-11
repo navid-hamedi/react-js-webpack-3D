4376913e7bd0ee267a6b4f361d63eb8a1f0e9e62
neanix <nav.hamedi@gmail.com>
2023-08-11 16:47:41 +0200

    three js file

---
+++ src/components/ModelViewer.js
@@ -0,0 +1,5 @@
+import React, { useEffect } from 'react';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import 'three/examples/jsm/controls/OrbitControls';
+import './ModelViewer.css';
